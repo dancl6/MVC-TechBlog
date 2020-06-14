70cd8a47bd30e33f4c82880dccacfa884380a581
dancl6 <danielclyons@yahoo.com>
2020-06-14 15:57:59 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  const sequelize = new Sequelize('mvc_tech_blog_db', 'root', 'mysql', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
