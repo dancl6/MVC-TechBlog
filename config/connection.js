ad97efcc3ace0e946f06a609ed3090f0e2f35941
dancl6 <danielclyons@yahoo.com>
2020-06-14 16:17:25 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const Sequelize = require('sequelize');
-
+// const sequelize;
 require('dotenv').config();
 
 // create connection to our db
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  const sequelize = new Sequelize('mvc_tech_blog_db', 'root', 'mysql', {
+
+  if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    // sequelize = new Sequelize('mvc_tech_blog_db', 'root', 'mysql', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
 });
-
+  }
 module.exports = sequelize;
